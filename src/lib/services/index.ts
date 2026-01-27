a90b06aa15dc1ba2f802204514cf506c606cfb54
alessiodanna <alessio.danna@skyscanner.net>
2026-01-27 09:55:47 +0100

    refactor for planning

--- src/lib/services/index.ts
+++
@@ -1,8 +0,0 @@
-export { DiceService, diceService } from './dice.service';
-export { RulesEngine, rulesEngine, PROFICIENCY_BONUS_BY_LEVEL, EXPERIENCE_THRESHOLDS } from './rules.engine';
-export { CharacterService, characterService } from './character.service';
-export { CampaignService, campaignService } from './campaign.service';
-export { CombatService, combatService } from './combat.service';
-export { SessionService, sessionService } from './session.service';
-export { AIService, aiService } from './ai.service';
-export { VoiceService, voiceService } from './voice.service';
